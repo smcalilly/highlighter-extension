5f90b61f1beb090d0e55bab9b64f7ec3ede41ef3
Sam McAlilly <sammcalilly@gmail.com>
2020-01-21 15:12:46 -0600

    taking a simpler approach without the forced login for now. code clean up.

--- scripts/background.js
+++ scripts/background.js
@@ -4,32 +4,13 @@ async function captureHighlight(textSelection) {
   if (jwt == null) {
     const notification = {
       title: 'Unable to save highlight!',
-      message: 'Please login to save!'
+      message: 'Please login to save.'
     }
-    createNotification(notification)
-    // jwt = await forceLogin()
-    // console.log(jwt);
-    // await postHighlight(formatHighlight(textSelection), jwt);
+    createNotification(notification);
   } else {
     postHighlight(formatHighlight(textSelection), jwt);
   }
 }
-
-// async function forceLogin(callback) {
-//   // open a small window so user can login, instead of the popup 
-//   // since chrome won't allow a popup to open programmatically
-//   await window.open(
-//     'ui/login.html',
-//     'extension_popup',
-//     'width=300,height=400,status=no,scrollbars=yes,resizable=no'
-//   );
-
-//   let jwt = await localStorage.getItem('highlighterJWT')
-
-//   console.log('forceLogin', jwt)
-
-//   return jwt;
-// }
 
 function formatHighlight(selection) {
   const highlight = {
@@ -81,28 +62,6 @@ function createNotification(notification) {
 
   chrome.notifications.create(message);
 }
-
-// function successMessage() {
-//   const message = {
-//     type: 'basic',
-//     iconUrl: 'images/yellow-box.png',
-//     title: 'Highlight saved!',
-//     message: 'Keep on reading.',
-//   }
-
-//   chrome.notifications.create(message);
-// }
-
-// function errorMessage(error) {
-//   const message = {
-//     type: 'basic',
-//     iconUrl: 'images/yellow-box.png',
-//     title: 'Unable to save highlight',
-//     message: 'This should not happen! Please contact us if the error persists.',
-//   }
-
-//   chrome.notifications.create(message);
-// }
 
 // add context menu
 chrome.runtime.onInstalled.addListener(function() {
