f751451bfbc39872d2091819de0b5e05ea29f898
Sam McAlilly <sammcalilly@gmail.com>
2020-01-21 15:07:54 -0600

    trying to force user to login and save the highlight after they login. it is tricy.

--- scripts/background.js
+++ scripts/background.js
@@ -1,4 +1,37 @@
-function captureHighlight(selection) {
+async function captureHighlight(textSelection) {
+  let jwt = localStorage.getItem('highlighterJWT');
+  
+  if (jwt == null) {
+    const notification = {
+      title: 'Unable to save highlight!',
+      message: 'Please login to save!'
+    }
+    createNotification(notification)
+    // jwt = await forceLogin()
+    // console.log(jwt);
+    // await postHighlight(formatHighlight(textSelection), jwt);
+  } else {
+    postHighlight(formatHighlight(textSelection), jwt);
+  }
+}
+
+// async function forceLogin(callback) {
+//   // open a small window so user can login, instead of the popup 
+//   // since chrome won't allow a popup to open programmatically
+//   await window.open(
+//     'ui/login.html',
+//     'extension_popup',
+//     'width=300,height=400,status=no,scrollbars=yes,resizable=no'
+//   );
+
+//   let jwt = await localStorage.getItem('highlighterJWT')
+
+//   console.log('forceLogin', jwt)
+
+//   return jwt;
+// }
+
+function formatHighlight(selection) {
   const highlight = {
     text: selection.selectionText, 
     url: selection.pageUrl
@@ -7,7 +40,7 @@ function captureHighlight(selection) {
   return highlight;
 }
 
-async function postFetch(highlight, jwt) {
+async function postHighlight(highlight, jwt) {
   const url = 'http://localhost:3000/highlights';
 
   const response = await fetch(url, {
@@ -21,36 +54,57 @@ async function postFetch(highlight, jwt) {
     body: JSON.stringify({ highlight: highlight })
   });
 
+  const notification = {}
+
   if (response.ok) {
-    successMessage();
+    notification = {
+      title: 'Highlight saved!',
+      message: 'Keep on reading.'
+    }
   } else {
-    errorMessage(response)
+    notification = {
+      title: 'Unable to save highlight!',
+      message: 'This should not happen. Please contact us if the error persists.'
+    }
   }
+
+  createNotification(notification);
 }
 
-function successMessage() {
+function createNotification(notification) {
   const message = {
     type: 'basic',
     iconUrl: 'images/yellow-box.png',
-    title: 'Highlight saved!',
-    message: 'Keep on reading.',
+    title: notification.title,
+    message: notification.message,
   }
 
   chrome.notifications.create(message);
 }
 
-function errorMessage(error) {
-  const message = {
-    type: 'basic',
-    iconUrl: 'images/yellow-box.png',
-    title: 'Unable to save highlight',
-    message: 'This should not happen! Please contact us if the error persists.',
-  }
+// function successMessage() {
+//   const message = {
+//     type: 'basic',
+//     iconUrl: 'images/yellow-box.png',
+//     title: 'Highlight saved!',
+//     message: 'Keep on reading.',
+//   }
 
-  chrome.notifications.create(message);
-}
+//   chrome.notifications.create(message);
+// }
 
-// add context menu and listen to text selection
+// function errorMessage(error) {
+//   const message = {
+//     type: 'basic',
+//     iconUrl: 'images/yellow-box.png',
+//     title: 'Unable to save highlight',
+//     message: 'This should not happen! Please contact us if the error persists.',
+//   }
+
+//   chrome.notifications.create(message);
+// }
+
+// add context menu
 chrome.runtime.onInstalled.addListener(function() {
   chrome.contextMenus.create({
     'id': 'addHighlight',
@@ -59,20 +113,7 @@ chrome.runtime.onInstalled.addListener(function() {
   });
 });
 
-// capture highlight and send it to the api
-chrome.contextMenus.onClicked.addListener(function (selection) {
-  const jwt = localStorage.getItem('highlighterJWT');
-
-  if (jwt == null) {
-    // chrome won't allow popup to open programmatically
-    // so open a small login window
-    window.open(
-      'ui/login.html', 
-      'extension_popup', 
-      'width=300,height=400,status=no,scrollbars=yes,resizable=no'
-    );
-  } else {
-    const highlight = captureHighlight(selection);
-    postFetch(highlight, jwt);
-  }
+// listen to contextMenu for the text selection event
+chrome.contextMenus.onClicked.addListener(function(textSelection) {
+  captureHighlight(textSelection);
 });
